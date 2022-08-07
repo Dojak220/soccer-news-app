0626836872b898ecc0260e453889a5bc3688bf0d
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-08-07 18:27:24 -0300

    Add RecyclerView and mock data to it

---
+++ app/src/main/java/me/dio/soccernews/domain/News.java
@@ -0,0 +1,27 @@
+package me.dio.soccernews.domain;
+
+public class News {
+    private String title;
+    private String subtitle;
+
+    public News(String title, String subtitle) {
+        this.title = title;
+        this.subtitle = subtitle;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+}
--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -4,16 +4,37 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import me.dio.soccernews.domain.News;
+
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<News>> newsData;
 
     public NewsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is news fragment");
+        newsData = new MutableLiveData<>();
+
+        /// TODO: Remove news mock
+        List<News> newsList = new ArrayList<>();
+        newsList.add(new News(
+                "Abelhas Rainhas iniciam treinos",
+                "Time picoense de futebol feminino iniciam treino e técnico mostra-se confiante."
+        ));
+        newsList.add(new News(
+                "Abelhas Rainhas ganham primeiro jogo",
+                "Time picoense vence partida contra Teresina por 2 a 1."
+        ));
+        newsList.add(new News(
+                "Abelhas Rainhas passam para a semifinal",
+                "O time passa para a semifinal do campeonato piauiense de futebol depois de vários jogos acirrados."
+        ));
+
+        newsData.setValue(newsList);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<News>> getNews() {
+        return newsData;
     }
 }
