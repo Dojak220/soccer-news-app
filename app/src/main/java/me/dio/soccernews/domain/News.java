9e1ee15562df959e385aa80538a97a46718793a4
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-08-07 21:02:21 -0300

    Add retrofit and picasso to conect API to app

--- app/src/main/java/me/dio/soccernews/domain/News.java
+++ app/src/main/java/me/dio/soccernews/domain/News.java
@@ -3,10 +3,14 @@ package me.dio.soccernews.domain;
 public class News {
     private String title;
     private String subtitle;
+    private String image;
+    private String link;
 
-    public News(String title, String subtitle) {
+    public News(String title, String subtitle, String image, String link) {
         this.title = title;
         this.subtitle = subtitle;
+        this.image = image;
+        this.link = link;
     }
 
     public String getTitle() {
@@ -23,5 +27,21 @@ public class News {
 
     public void setSubtitle(String subtitle) {
         this.subtitle = subtitle;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
     }
 }
--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -1,38 +1,57 @@
 package me.dio.soccernews.ui.news;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import me.dio.soccernews.data.remote.SoccerNewsAPI;
 import me.dio.soccernews.domain.News;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<List<News>> newsData;
+    private final MutableLiveData<List<News>> newsData = new MutableLiveData<>();
+    private final SoccerNewsAPI api;
 
     public NewsViewModel() {
-        newsData = new MutableLiveData<>();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://dojak220.github.io/soccer-news-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        /// TODO: Remove news mock
-        List<News> newsList = new ArrayList<>();
-        newsList.add(new News(
-                "Abelhas Rainhas iniciam treinos",
-                "Time picoense de futebol feminino iniciam treino e técnico mostra-se confiante."
-        ));
-        newsList.add(new News(
-                "Abelhas Rainhas ganham primeiro jogo",
-                "Time picoense vence partida contra Teresina por 2 a 1."
-        ));
-        newsList.add(new News(
-                "Abelhas Rainhas passam para a semifinal",
-                "O time passa para a semifinal do campeonato piauiense de futebol depois de vários jogos acirrados."
-        ));
+        api = retrofit.create(SoccerNewsAPI.class);
 
-        newsData.setValue(newsList);
+        api.getNews().enqueue(handleCallback());
     }
+
+    @NonNull
+    private Callback<List<News>> handleCallback() {
+        return new Callback<List<News>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<News>> call, @NonNull Response<List<News>> response) {
+                if (response.isSuccessful()) {
+                    newsData.setValue(response.body());
+                } else {
+                    /// TODO: Create error handler
+                    /// showErrorMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<News>> call, @NonNull Throwable t) {
+                /// TODO: Create error handler
+            }
+        };
+    }
+
+
 
     public LiveData<List<News>> getNews() {
         return newsData;
